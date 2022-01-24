c9b705522f0f130f34dbed42ca441b019d671327
Duy Huynh <huynhtruongdyu@gmail.com>
2022-01-24 14:42:23 +0700

    Add project files.

---
+++ src/Core/Caroline.Domain/Auditable/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿namespace Caroline.Domain.Auditable
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public string UpdatedBy { get; set; }
+        public bool DeleteFlag { get; set; }
+
+        public BaseEntity()
+        {
+            CreatedDate = DateTime.UtcNow;
+            DeleteFlag = false;
+        }
+    }
+}
---
+++ src/Core/Caroline.Domain/Categories/Post.cs
@@ -0,0 +1,11 @@
+﻿using Caroline.Domain.Auditable;
+
+namespace Caroline.Domain.Categories
+{
+    public class Post : BaseEntity
+    {
+        public string Title { get; set; }
+        public string Thumbnail { get; set; }
+        public string Content { get; set; }
+    }
+}
